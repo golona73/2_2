ef99caaaaad33e0392c82ec8a06868c1f5b1a70e
BOOMER <127319193+golona73@users.noreply.github.com>
2023-03-22 14:13:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+  
+        
+        float a=2;
+  float b=5;
+        float dodawanie=a+b;
+ float odejmowanie=a-b;
+ float mnozenie=a*b;
+  float dzielenie=a/b;
+  System.out.println("SUMA: " + dodawanie+ " RÓŻNICA: " +odejmowanie + " MNOŻENIE: " + mnozenie+ " ILORAZ: " + dzielenie );
+}
+
+  
+}
